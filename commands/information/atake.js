0a12159297ec3c6d9c30d6e602c8336338c4f020
thiccsupreme <57068146+thiccsupreme@users.noreply.github.com>
2020-03-06 22:04:00 -0500

    Update atake.js

--- commands/information/atake.js
+++ commands/information/atake.js
@@ -8,7 +8,7 @@ module.exports = {
         const embed = new RichEmbed()
         .setColor("E66CA5")
         .addField(" 🌎☄️💕", "[👽🛸Click at your own risk🛸👽🛸](https://open.spotify.com/playlist/0c14jmmx6VEe3EgFnwsrZY?si=mXCysfI0SOufN_SGyuj9MA)")
-        .setFooter("thiccsupeme is a hi roller")
+        .setFooter("thiccsupeme is a hi roller ⛸️")
         message.channel.send(embed);
 
     }}
